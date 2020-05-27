38e1347b119da759b9ade9f8c9efecfa2d04e159
bea92 <bea92@DESKTOP-84TUKSR>
2020-05-27 14:03:54 +0200

    first commit bea

---
+++ src/main/java/dd/sample/hello2/App.java
@@ -0,0 +1,13 @@
+package dd.sample.hello2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Beaaaa!!!!!!!!!!!!!!" );
+    }
+}
